3b2268f3a8bcb8c32f0be6ded42a91fa02794bc3
Borkes <1043568089@qq.com>
2018-05-20 15:49:59 +0800

    range续载

--- httpserver.js
+++ httpserver.js
@@ -18,7 +18,7 @@ function handleRequest(req, res) {
     let pathName = url.parse(req.url).pathname;
     /**根据pathName，路由调用不同处理逻辑 */
     if (pathName === '/') {
-        resIndex(res); //响应HTML页面到Web客户端
+        resIndex(req, res); //响应HTML页面到Web客户端
     } else if (pathName.match('/dist/')) {
         resImage(req, res);
     } else if (pathName.match('/download-linux')) {
@@ -30,7 +30,21 @@ function handleRequest(req, res) {
 }
 
 //首页
-function resIndex(res) {
+function resIndex(req, res) {
+    function compressHandle(raw, statusCode, reasonPhrase) {
+        var stream = raw;
+        var acceptEncoding = req.headers['accept-encoding'] || "";
+        var matched = ext.match(config.Compress.match);
+        if (matched && acceptEncoding.match(/\bgzip\b/)) {
+            res.setHeader("Content-Encoding", "gzip");
+            stream = raw.pipe(zlib.createGzip());
+        } else if (matched && acceptEncoding.match(/\bdeflate\b/)) {
+            res.setHeader("Content-Encoding", "deflate");
+            stream = raw.pipe(zlib.createDeflate());
+        }
+        res.writeHead(statusCode, reasonPhrase);
+        stream.pipe(res);
+    };
     fs.readdir('./dist', function (err, files) {
         if (err) {
             return res.end('error');
@@ -38,20 +52,39 @@ function resIndex(res) {
         let pictures = files.map(file => {
             return '/dist/' + file;
         })
-        let expires = new Date();
-        expires.setTime(expires.getTime() + 1000 * 1000);
-        res.writeHead(200, {
-            "Content-Type": "text/html",
-            "content-encoding": "gzip",
-            "Expires": expires.toUTCString(),
-            "Cache-Control": "max-age=3000",
-        });
-        const inStream = new Readable();
-        inStream.push(index(pictures));
-        inStream.push(null); // 没有更多数据了
-        inStream.pipe(zlib.createGzip()).pipe(res);
-        //res.writeHead(200, { "Content-Type": "text/html" });
-        //res.end(index(pictures)); //传递基于dist目录的图片数组
+
+        //如果有断点续载,根据需要优化，暂时关闭
+        if (0 && req.headers["range"]) {
+            var range = parseRange(req.headers["range"], stats.size);
+            if (range) {
+                res.setHeader("Content-Range", "bytes " + range.start + "-" + range.end + "/" + stats.size);
+                res.setHeader("Content-Length", (range.end - range.start + 1));
+                var raw = fs.createReadStream(realPath, {
+                    "start": range.start,
+                    "end": range.end
+                });
+                compressHandle(raw, 206, "Partial Content");
+            } else {
+                res.removeHeader("Content-Length");
+                res.writeHead(416, "Request Range Not Satisfiable");
+                res.end();
+            }
+        } else {
+            let expires = new Date();
+            expires.setTime(expires.getTime() + 1000 * 1000);
+            res.writeHead(200, {
+                "Content-Type": "text/html",
+                "content-encoding": "gzip",
+                "Expires": expires.toUTCString(),
+                "Cache-Control": "max-age=3000",
+            });
+            const inStream = new Readable();
+            inStream.push(index(pictures));
+            inStream.push(null); // 没有更多数据了
+            inStream.pipe(zlib.createGzip()).pipe(res);
+            //res.writeHead(200, { "Content-Type": "text/html" });
+            //res.end(index(pictures)); //传递基于dist目录的图片数组
+        }
     })
 }
 
@@ -100,10 +133,36 @@ function download(req, res) {
     zipProc.stdout.pipe(res)
     zipProc.stderr.on('data', function (data) {
         console.log(data.toString())
-        res.writeHead(404, {'Content-Type': 'text/plain'})
+        res.writeHead(404, { 'Content-Type': 'text/plain' })
         res.end();
     })
     zipProc.on('close', function (code) {
         console.log(code)
     })
+}
+
+//断点续载范围处理
+function parseRange(str, size) {
+    if (str.indexOf(",") != -1) {
+        return;
+    }
+    var range = str.split("-"),
+        start = parseInt(range[0], 10),
+        end = parseInt(range[1], 10);
+    // Case: -100
+    if (isNaN(start)) {
+        start = size - end;
+        end = size - 1;
+        // Case: 100-
+    } else if (isNaN(end)) {
+        end = size - 1;
+    }
+    // Invalid
+    if (isNaN(start) || isNaN(end) || start > end || end > size) {
+        return;
+    }
+    return {
+        start: start,
+        end: end
+    };
 }
